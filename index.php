65c0317c3183de8bfaf19b85711be9e942403504
Hilmi Raditya <raditya113@icloud.com>
2018-05-03 18:28:03 +0700

    Beres

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Coming Soon</title>
+	<link href="https://fonts.googleapis.com/css?family=Archivo" rel="stylesheet"> 
+</head>
+<style>
+body  {
+  font-family: 'Archivo', sans-serif;
+  background-image: url('images/background.jpg');
+  background-repeat: no-repeat;
+}
+
+.cons{
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  top: 30%;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  height: 2em;
+  width: 100%;
+  text-align: center;
+}
+
+.text {
+	color: white;
+}
+
+</style>
+<body>
+	<div class="cons">
+		<p class="text" style="font-size: 60px;">Coming Soon</p>
+		<p class="text">August 2018 Test</p>
+	</div>
+	<footer>
+		<p class="text"> &copy; <b>2018</b> - Hilmi Raditya</p>
+	</footer>
+</body>
+</html>
